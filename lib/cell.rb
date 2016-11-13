e7015149ac6d4b97c614fc676c3cefc90ec12509
Orion Osborn <oosborn@invoca.com>
2016-11-13 11:37:51 -0800

    add additional cell assertions

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Cell
   attr_reader :state
 
@@ -6,6 +8,12 @@ class Cell
   end
 
   def step(neighbors:)
-    @state = 0
+    alive_neighbors = neighbors.count {|value| value == 1}
+    if state == 1
+      case alive_neighbors
+      when 0, 1, 4, 5, 6, 7, 8
+        @state = 0
+      end
+    end
   end
 end
--- test/cell_test.rb
+++ test/cell_test.rb
@@ -3,13 +3,63 @@ require 'minitest/pride'
 require_relative '../lib/cell.rb'
 
 class CellTest < MiniTest::Unit::TestCase
-  def test_cell_dies_if_has_one_neighbor
+  def test_cell_dies_if_has_no_neighbors
     cell = Cell.new(state: 1)
-    neighbors = Array.new(8, 0)
-    neighbors[0] = 1
+    neighbors = create_neighbors(number_of_cells_on: 0)
 
     cell.step(neighbors: neighbors)
 
     assert_equal 0, cell.state
+  end
+
+  def test_cell_dies_if_has_one_neighbor
+    cell = Cell.new(state: 1)
+    neighbors = create_neighbors(number_of_cells_on: 1)
+
+    cell.step(neighbors: neighbors)
+
+    assert_equal 0, cell.state
+  end
+
+  def test_cell_lives_if_has_two_neighbors
+    cell = Cell.new(state: 1)
+    neighbors = create_neighbors(number_of_cells_on: 2)
+
+    cell.step(neighbors: neighbors)
+
+    assert_equal 1, cell.state
+  end
+
+  def test_cell_lives_if_has_three_neighbors
+    cell = Cell.new(state: 1)
+    neighbors = create_neighbors(number_of_cells_on: 3)
+
+    cell.step(neighbors: neighbors)
+
+    assert_equal 1, cell.state
+  end
+
+  def test_cell_dies_if_has_four_neighbors
+    cell = Cell.new(state: 1)
+    neighbors = create_neighbors(number_of_cells_on: 4)
+
+    cell.step(neighbors: neighbors)
+
+    assert_equal 0, cell.state
+  end
+
+  def test_cell_dies_if_has_five_neighbors
+    cell = Cell.new(state: 1)
+    neighbors = create_neighbors(number_of_cells_on: 5)
+
+    cell.step(neighbors: neighbors)
+
+    assert_equal 0, cell.state
+  end
+
+  def create_neighbors(number_of_cells_on:)
+    on = Array.new(number_of_cells_on, 1)
+    off = Array.new(8 - number_of_cells_on, 0)
+    on + off
   end
 end
